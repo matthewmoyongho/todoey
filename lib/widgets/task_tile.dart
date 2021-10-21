ec3d170f1821a4ef4e8fca5094eb68eedb9581c0
John Brave <moyonghomatthew@gmail.com>
2021-10-21 19:24:13 +0100

    more update

---
+++ lib/widgets/task_tile.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/widgets/task_check_box.dart';
+
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool? isChecked = false;
+  late bool decorate;
+
+  // TextStyle textDecoration() {
+  //   if (isChecked == null) {
+  //     return TextStyle(decoration: TextDecoration.lineThrough);
+  //   } else {
+  //     return TextStyle(decoration: TextDecoration.none); ;
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text(
+        'Do this',
+        style: TextStyle(
+            decoration:
+                isChecked ? TextDecoration.lineThrough : TextDecoration.none),
+      ),
+      trailing: TaskCheckBox(
+        isCheched: isChecked,
+        onPressed: (val) {
+          setState(() {
+            isChecked = val;
+          });
+        },
+      ),
+    );
+  }
+}
